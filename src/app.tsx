228d4710ac59486d6aab5d1f4902ffcf419305a7
Matheus Borges Teixeira <matheus.bt@gmail.com>
2021-03-06 20:15:00 +0100

    Adding evergreen.

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,15 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Button } from 'evergreen-ui';
+import AdvancedTable from './AdvancedTable';
 
 function render() {
   console.log("Rendering...")
-  ReactDOM.render(<h2>Hello from React!</h2>, document.getElementById("internal-app"));
+  ReactDOM.render(<div>
+    <h2>Hello from React!</h2>
+    <Button>I am using 🌲 Evergreen!</Button>
+    <AdvancedTable />
+    </div>, document.getElementById("internal-app"));
 }
 
 render();
