c0f4c819dcba587a368aade2633c73c1cb21ad0b
Matheus Borges Teixeira <matheus.bt@gmail.com>
2024-04-02 19:44:57 +0200

    Upgrading to latest greatest.

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,10 @@
 import { createStore, applyMiddleware } from "redux";
 import reducer from "./reducers";
 import { Currency } from './models/Activity'
-import thunkMiddleware from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
+import { thunk } from 'redux-thunk'
+import { composeWithDevTools } from '@redux-devtools/extension'
 
-const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware))
+const composedEnhancer = composeWithDevTools(applyMiddleware(thunk))
 
 function configureStore(state: any = {}) {
   return createStore(reducer, state, composedEnhancer);
