b020e402eae6b523483fad44b08639e890f2246d
Matheus Borges Teixeira <matheus.bt@gmail.com>
2026-01-31 17:45:41 +0100

    Review.

--- tests/setupTests.ts
+++ tests/setupTests.ts
@@ -2,11 +2,20 @@
  * @jest-environment jsdom
  */
 
-// @ts-ignore
-window.storage = {
-    appendData: jest.fn(),
-    loadAllFiles: jest.fn(),
-    listenData: jest.fn(),
+declare global {
+  interface Window {
+    storage: {
+      appendData: jest.Mock;
+      loadAllFiles: jest.Mock;
+      listenData: jest.Mock;
+    };
+  }
 }
+
+window.storage = {
+  appendData: jest.fn(),
+  loadAllFiles: jest.fn(),
+  listenData: jest.fn(),
+};
 
 import '@testing-library/jest-dom';
